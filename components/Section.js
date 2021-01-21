f527cf1aee175906d3706b3ea7e4b2df3bc446f1
Tomek Jankowski <tomasz.jankowski@idea-inspired.com>
2021-01-22 00:09:06 +0100

    Refactor section component to use container component

--- components/Section.js
+++ components/Section.js
@@ -2,18 +2,19 @@ import clsx from "clsx";
 import styles from "styles/Section.module.scss";
 import layoutStyles from "styles/Layout.module.scss";
 import commonStyles from "styles/Common.module.scss";
+import Container from "./Container";
 
 export default function Section({ title, className, children }) {
   return (
     <div className={clsx(styles.section, className)}>
-      <div className={layoutStyles.container}>
+      <Container>
         {title && (
           <h3 className={clsx(commonStyles.highlight, styles.section__title)}>
             {title}
           </h3>
         )}
         {children}
-      </div>
+      </Container>
     </div>
   );
 }
