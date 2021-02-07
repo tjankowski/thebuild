1cc8d1e51a0738f6e06f1f488d2c2de25a7ae10b
Tomek Jankowski <tomasz.jankowski@idea-inspired.com>
2021-02-07 17:55:13 +0100

    Refactor navigation to remove repeated code

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,23 +1,37 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
+import clsx from "clsx";
+
+class Page {
+  constructor(label, link) {
+    this.label = label;
+    this.link = link;
+  }
+}
+
+const pages = [
+  new Page("Notes", "/notes"),
+  new Page("Lab", "/lab"),
+  new Page("About", "/about"),
+];
 
 export default function Navigation() {
+  const router = useRouter();
   return (
     <ul className="navigation">
-      <li>
-        <Link href="/notes">
-          <a>Notes</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/lab">
-          <a>Lab</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </li>
+      {pages.map((page) => (
+        <li key={page.link}>
+          <Link href={page.link}>
+            <a
+              className={clsx("highlight", {
+                highlight_hover: page.link !== router.pathname,
+              })}
+            >
+              {page.label}
+            </a>
+          </Link>
+        </li>
+      ))}
     </ul>
   );
 }
